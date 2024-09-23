b32d7554d2325181864b123d38bcab9b6a254051
minhphu102003 <minhphu01200@gmail.com>
2024-09-23 23:27:55 +0700

    optimizing function

--- bedao/demDiem.cpp
+++ bedao/demDiem.cpp
@@ -35,6 +35,45 @@ void solve(){
     }
 }
 
+void optimizing (){
+    int n,q;
+    cin>>n>>q;
+    vector<pair<ll,int>> list(n);
+    int x,y,w;
+    for (int  i = 0; i < n; i++)
+    {
+        cin>>x>>y>>w;
+        list[i].first = (ll)x*x + (ll)y*y;
+        list[i].second = w;
+    }
+    sort(list.begin(), list.end()); // sort ở đây để phục vụ tìm kiếm nhị phân
+    vector<ll> preFix(n+1); // hàm trên sort thì hàm dưới này chắc chắn cũng sort theo
+    for (int  i = 0; i < n; i++)
+    {
+        preFix[i+1] = preFix[i] + list[i].second;
+    }
+    while (q--)
+    {
+        ll r;
+        cin>>r;
+        r *= r;
+        int ans = -1;
+        int low = 0, high = n-1;
+        while (low <=high)
+        {
+            int mid = (low + high)/2;
+            if(list[mid].first <= r){
+                ans = mid;
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        cout<<preFix[ans + 1]<<"\n";  
+    }
+}
+
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
@@ -42,6 +81,7 @@ int main(){
     // freopen("B001.inp","r",stdin);
     // freopen("B001.out","w",stdout);
     
-    solve();  // Gọi hàm solve() đã chỉnh sửa
+    //solve();  // Gọi hàm solve() đã chỉnh sửa
+    optimizing();
     return 0;
 }
