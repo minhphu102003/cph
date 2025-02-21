682fa6091b9ad2746fae38293cd95016892b2745
minhphu102003 <minhphu01200@gmail.com>
2025-02-21 23:41:56 +0700

    solve problem 6 in leetcode

--- leetcode/6.cpp
+++ leetcode/6.cpp
@@ -4,42 +4,21 @@
 using namespace std;
 
 string convert(string s, int numRows) {
-    int len = s.length();
-    int currentIndex = 0;
-    if (len < currentIndex) return "";
+    if (numRows == 1 || s.length() <= numRows) return s;
+
+    vector<string> rows(min(numRows, int(s.length())));
+    int currentRow = 0;
+    bool goingDown = false;
+
+    for (char c : s) {
+        rows[currentRow] += c;
+        if (currentRow == 0 || currentRow == numRows - 1)
+            goingDown = !goingDown;
+        currentRow += goingDown ? 1 : -1;
+    }
+
     string result;
-    result.push_back(s[currentIndex]);
-    cout<< s[currentIndex];
-    // Tách trường hợp đặc biệt ra
-    int loop = 2;
-    while (currentIndex < len)
-    {
-        currentIndex = loop*numRows -4 + loop;
-        loop++;
-        if (len < currentIndex) break;
-        result.push_back(s[currentIndex]);
-        cout<< s[currentIndex];
-    }
-    int inter = 1;
-    int step = 2 * numRows - 2;
-    while (inter+1 < numRows)
-    {
-        int firstInter = inter;
-        int secondInter = 2*numRows  - 2 - inter;
-        while (firstInter < len)
-        {
-            result.push_back(s[firstInter]);
-            cout<< s[firstInter];
-            firstInter += step;
-        }
-        while (secondInter < len)
-        {
-            result.push_back(s[secondInter]);
-            cout<< s[secondInter];
-            secondInter += step;
-        }
-        inter++;
-    }
+    for (string row : rows) result += row;
     return result;
 }
 
