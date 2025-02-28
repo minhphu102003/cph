ed993bbd7a6f632929e43f780bcc51f7bf6bde9f
minhphu102003 <minhphu01200@gmail.com>
2025-02-28 22:55:11 +0700

    solve problem 15 in leetcode

---
+++ leetcode/15.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+    sort(nums.begin(), nums.end()); 
+    vector<vector<int>> result;
+    int n = nums.size();
+    for (int i = 0; i < n - 2; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue; 
+        int left = i + 1, right = n - 1;
+        while (left < right) {
+            int sum = nums[i] + nums[left] + nums[right];
+            if (sum == 0) { 
+                result.push_back({nums[i], nums[left], nums[right]});
+                left++;
+                right--;
+                while (left < right && nums[left] == nums[left - 1]) left++;
+                while (left < right && nums[right] == nums[right + 1]) right--;
+            }
+            else if (sum < 0) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+    }
+    return result;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    freopen("B001.inp","r",stdin);
+    freopen("B001.out","w",stdout);
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin>>nums[i];
+    }
+    vector<vector<int>> result = threeSum(nums);
+    for (int i = 0; i < result.size(); i++)
+    {
+        for (int j = 0; j < result[i].size(); j++)
+        {
+            cout<<result[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
